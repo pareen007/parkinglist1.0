8b86921f1a1eae348163c706bc87aef203105cbe
Pareen Patel <pareendpatel@gmail.com>
2019-08-31 05:14:27 -0700

    Added basic starter files

---
+++ js/landingPage.js
@@ -0,0 +1,24 @@
+
+sessionStorage.clear();
+if (sessionStorage.getItem('location') === null) {
+    sessionStorage.setItem('location', '95123')
+}
+
+////////////////
+mapboxgl.accessToken = 'pk.eyJ1IjoicGFyZWVuODkiLCJhIjoiY2p6bXlrb3F0MGQwOTNwczZkNDR1M2V0biJ9.wnH7WAPDltqrQmWTmTrbLg';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+});
+//////////////////
+
+var geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+});
+
+geocoder.on('result', function(e){
+    sessionStorage.setItem('location', e.result.place_name)
+})
+
+document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
